20f1374afbe6a7681bb5af8deb7a62f1defae751
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-05-10 13:27:26 -0400

    Initial Commit

---
+++ ObjectiveC-Intro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveC-Intro
+//
+//  Created by Brendon Crowe on 5/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
